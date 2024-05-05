3ac34325bfdeb7acfe7cee1b96b631f34ac306e4
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-05-05 15:13:47 +0530

    HavenHub version2 updated

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,35 +1,9 @@
-import { Route, Routes } from "react-router-dom";
-import Layout from "./Layout";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
-import Navbar from "./Pages/Navbar";
-import AddPlace from "./Pages/AddPlace";
-import Place from "./Pages/Places";
-import CardDetail from "./Pages/CardDetail";
-import ReservePlace from "./Pages/ReservePlace";
-import { AuthProvider } from "./AuthContext";
-import Bookings from "./Pages/Bookings";
-import UpdateCard from "./Pages/UpdateCard";
-import Page404 from "./Pages/page404";
+import React from 'react'
 
 function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/addplace" element={<AddPlace />} />
-        <Route path="/place" element={<Place />} />
-        <Route path="/card-detail/:id?" element={<CardDetail />} />
-        <Route path="/reserve-place" element={<ReservePlace />} />
-        <Route path="/booking" element={<Bookings />} />
-        <Route path="/updatecard/:id?" element={<UpdateCard />} />
-        <Route path="*" element={<Page404/>} />
-
-      </Routes>
-    </AuthProvider>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App

dcd53bdfccef8bc503ab90d97d2416eb97da451e
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-05-05 10:14:14 +0530

    images removed server updated

--- api/index.js
+++ api/index.js
@@ -4,10 +4,10 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const User = require("./models/Users");
-const SECRET_KEY = "airbnbsecretkey";
+const SECRET_KEY = "turtlewaxbyurbangaragesecretkey";
 const bcrypt = require("bcrypt");
 const multer = require("multer");
-const Place = require("./models/Place");
+
 // image downloader
 const axios = require("axios");
 const fs = require("fs");

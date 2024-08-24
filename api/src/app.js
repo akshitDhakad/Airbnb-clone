2e590cff9b3d054ab15734d034d17b984cea8770
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-08-25 00:14:27 +0530

    database connected

--- api/src/app.js
+++ api/src/app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const cookiesParser = require("cookie-parser");
-
+const path = require("path");
 const app = express();
 
-
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CLIENT_URL,
-    credentials: true
-}));
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname,"public"));
+app.use(cookiesParser());
+
+// routes import
+
+const authRoutes = require("./routes/auth.route.js");
+const userRoutes = require("./routes/user.route.js");
+
+//routes declaration
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./db/index.js");
-const path = require("path");
+const dotenv = require("dotenv");
 // const router = express.Router();
 const cors = require("cors");
 const app = express();
@@ -10,40 +10,14 @@ dotenv.config({
 });
 
 
-// const bcrypt = require("bcrypt");
-const authRoutes = require("./routes/auth.route.js");
-const userRoutes = require("./routes/user.route.js");
 
 
-
-
-// middlewares
-const morgan = require("morgan");
-// middlewares
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
-
-
-
-
-// Routes 
-// Define authentication routes
-app.use('/auth', authRoutes);
-// Define user routes
-app.use('/user', userRoutes);
-
-
-
-// Connect to MongoDB
-connectDB();
-
-
-
-try {
-  app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+     console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
   });
-} catch (e) {
-  console.log(e);
-}
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!! ", err);
+})
